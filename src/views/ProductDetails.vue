1150289a3c90d0e98793a40c6b570d43ef362fa7
Hannah Ejimofor <hejimofor141@uniport.edu.ng>
2021-07-11 18:34:19 +0100

    project details page ui build

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -18,11 +18,20 @@
           <li>Sales 25% Off Use Code: Anna2021</li>
         </ul>  
         <p>{{ product.description }}</p>
-        <p><i class="far fa-heart text-red-500 mr-3"></i>ADD TO WISHLIST</p>
-        <form action="">
-          <input type="number">
-          <input type="submit" value="Add To Cart">
-        </form>
+        <p v-if="!wishListProduct"><i @click="wishListProduct = true" class="far fa-heart text-red-500 text-xl mr-2 cursor-pointer"></i>ADD TO WISHLIST</p>
+        <p v-if="wishListProduct"><i @click="wishListProduct = false" class="fas fa-heart text-red-500 text-xl mr-2 cursor-pointer"></i>REMOVE FROM WISHLIST</p>
+        <div class="addToCart" action="">
+          <div class="amt">
+            <button class="focus:outline-none">-</button>
+            <span>{{ itemQuantity }}</span>
+            <button class="focus:outline-none">+</button>
+          </div> 
+          <button class="btn focus:outline-none">Add To Cart</button>
+        </div>
+        <p>Category: {{ product.category }}</p>
+        <p>Brand: {{ product.brand }}</p>
+        <p>Tags:</p>
+        <li v-for="tag in product.tag_list" :key="tag.index">{{ tag }}</li>
       </div>     
     </div>
 
@@ -46,6 +55,8 @@
     data() {
       return {
         id: this.$route.params.id,
+        itemQuantity: '01',
+        wishListProduct: false 
       }
     },
 
@@ -71,6 +82,7 @@
     display: flex;
     align-items: flex-start;
     justify-content: space-between;
+    font-size: 14px;
   }
 
   .product-detail img {
@@ -86,7 +98,6 @@
     flex-direction: column;
     align-items: flex-start;
   }
-
   .product-info p:nth-child(1) {
     font-size: 25px;
     font-weight: bold;
@@ -95,11 +106,7 @@
     font-size: 20px;
     margin-top: 17px;
   }
-  .product-info p:nth-child(5) {
-    font-size: 14px;
-  }
   .product-info p:nth-child(6) {
-    font-size: 14px;
     margin-top: 20px;
   }
   .product-info ul {
@@ -111,4 +118,33 @@
     list-style-type: disc;
   }
 
+  .addToCart {
+    margin: 20px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .addToCart .btn {
+    padding: 10px 33px;
+    background: coral;
+    border-radius: 4px;
+    color: #fff;
+  }
+
+  .addToCart .amt {
+    width: 180px;
+    padding: 10px;
+    background: transparent;
+    border-radius: 4px;
+    border: 1px solid black;
+    margin-right: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+
+ 
 </style>
